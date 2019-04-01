a9858da19ac2226403006287d3b8ce7a67c0bade
Brian Carroll <briancarroll112@gmail.com>
2019-04-01 10:03:08 -0400

    add nav to homepage

--- client/src/components/BudgetRegister.jsx
+++ client/src/components/BudgetRegister.jsx
@@ -1,8 +1,4 @@
-//Second step of registering. user gives weekly budget
-// calls the PUT to update budget
-// can also be used when clicking settings to re-PUT(?) Budget
 import React from 'react';
-
 
 const BudgetRegister = (props) => {
 
